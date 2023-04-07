8a7c4cd3652abe9d2e8790fbe640ca0d251c8c51
Franck LAMY <franck.lamy39@free.fr>
2023-04-07 18:38:00 +0200

    Updates

--- index.12ca3f73.js
+++ index.12ca3f73.js
@@ -1,2 +1,2 @@
 var o=o=>{console.log(`Bonjour mon ami ${o} !`),console.log("Comment vas-tu ?")};o("Jean"),o("Pierre");
-//# sourceMappingURL=index.601fcb8d.js.map
+//# sourceMappingURL=index.12ca3f73.js.map
