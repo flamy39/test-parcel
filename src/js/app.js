05b038e4d9df82c31cd5d323aec22e6ac2bc8d8f
Franck LAMY <franck.lamy39@free.fr>
2023-04-07 15:11:13 +0200

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+// Importer le module bonjour
+import direBonjour from "./bonjour.js";
+
+direBonjour('Jean');
+direBonjour('Pierre');
---
+++ src/js/bonjour.js
@@ -0,0 +1,5 @@
+const direBonjour = (nom) => {
+  console.log(`Bonjour mon ami ${nom} !`);
+  console.log("Comment vas-tu ?")
+}
+export default direBonjour;
